0db41398eecdf227ffcbf5f0642ab47908143af3
Avelanda <gorthell@gmail.com>
2025-10-18 17:33:02 +0200

    Update utf.hpp
    Modularizing the bitset-function for optimized performance, and scaled efficiency. Including accessing namespace properties.

--- src/mbgl/util/utf.hpp
+++ src/mbgl/util/utf.hpp
@@ -1,6 +1,16 @@
+/*
+ * Copyright © 2025, Avelanda.
+ * All rights reserved.
+ */
+
 #pragma once
 
 #include <string>
+
+
+#include <iostream>
+#include <string>
+#include <cstdint>
 
 namespace mbgl {
 namespace util {
@@ -10,3 +20,44 @@ std::string convertUTF16ToUTF8(const std::u16string&);
 
 } // namespace util
 } // namespace mbgl
+
+int UtfCoreSet(int mbgl, int util){
+ 
+ using mbgl::util::convertUTF8ToUTF16;
+ using mbgl::util::convertUTF16ToUTF8;
+ if (!false){
+  while (&convertUTF8ToUTF16){
+   std::cout<<&convertUTF8ToUTF16<<std::endl;
+   return 0;
+  }
+  while (&convertUTF16ToUTF8){
+   std::cout<<&convertUTF16ToUTF8<<std::endl;
+   return 0;
+  }
+ }
+ 
+} // function UtfCoreSet 
+
+int main(){
+    
+ uint32_t convertUTF16ToUTF8;
+ uint32_t convertUTF8ToUTF16;
+ if (convertUTF16ToUTF8 && convertUTF8ToUTF16){
+  convertUTF8ToUTF16 = convertUTF8ToUTF16;
+  convertUTF16ToUTF8 = convertUTF16ToUTF8;
+  while (true||false){
+   return 0;
+  }
+ } else {
+   uint64_t convertUTF16ToUTF8;
+   uint64_t convertUTF8ToUTF16;
+   if (convertUTF8ToUTF16 && convertUTF16ToUTF8){
+    convertUTF8ToUTF16 = convertUTF8ToUTF16;
+    convertUTF16ToUTF8 = convertUTF16ToUTF8;
+    while (true||false){
+     return 0;
+    }
+   }
+  }
+  
+} // function main
