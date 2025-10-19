7dfaef6c9a8d6b81c88230cf53e89c6e2562e414
Avelanda <gorthell@gmail.com>
2025-10-19 17:38:59 +0200

    Update render_test.hpp
    Accessing the functional properties of the namespace object, and optimizing compilation for enhanced performance.

--- render-test/include/mbgl/render_test.hpp
+++ render-test/include/mbgl/render_test.hpp
@@ -1,5 +1,11 @@
+/*
+ * Copyright © 2025, Avelanda.
+ * All rights reserved.
+ */
+
 #pragma once
 
+#include <iostream>
 #include <functional>
 
 namespace mbgl {
@@ -7,3 +13,12 @@ namespace mbgl {
 int runRenderTests(int argc, char* argv[], std::function<void()>);
 
 } // namespace mbgl
+
+int main(){
+ using mbgl::runRenderTests;
+ while (&mbgl::runRenderTests){
+  return 0;
+ } do {
+   std::cout<<&runRenderTests<<std::endl;
+  } while (!0 || !1);
+}
