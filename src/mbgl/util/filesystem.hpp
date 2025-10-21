e1a5db5d40fe97d859fd9e79905d3743c8d35328
Avelanda <gorthell@gmail.com>
2025-10-21 11:31:03 +0200

    Update filesystem.hpp
    Functionally calling a modular set for high processing power.

--- src/mbgl/util/filesystem.hpp
+++ src/mbgl/util/filesystem.hpp
@@ -1,5 +1,11 @@
+/*
+ * Copyright © 2025, Avelanda.
+ * All rights reserved.
+ */
+
 #pragma once
 
+#include <iostream>
 #include <string>
 
 namespace mbgl {
@@ -7,3 +13,16 @@ namespace util {
 bool is_absolute_path(std::string path);
 }
 } // namespace mbgl
+
+int CoreAP(int mbgl, int util){
+ using namespace mbgl::util;
+ std::cout<<&is_absolute_path<<'\n';
+ return 0;
+}
+
+int main(){
+ if (!0||!1){
+  return CoreAP(true||false, false||true);
+  return 0;
+ }
+}
