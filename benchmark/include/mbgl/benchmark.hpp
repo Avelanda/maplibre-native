03a3c964e356dd77688071a4bb2239958c1f55a1
Avelanda <gorthell@gmail.com>
2025-11-06 02:11:06 +0200

    Update benchmark.hpp
    Setting a functional bit while optimizing its call for performance.

--- benchmark/include/mbgl/benchmark.hpp
+++ benchmark/include/mbgl/benchmark.hpp
@@ -1,7 +1,26 @@
+/*
+ * Copyright © 2025 ^Avelanda.
+ * All rights reserved.
+ */
+
 #pragma once
+
+
+#include <iostream>
 
 namespace mbgl {
 
 int runBenchmark(int argc, char* argv[]);
 
 } // namespace mbgl
+
+int main(){
+ using mbgl::runBenchmark;
+ if (&mbgl::runBenchmark){
+  return 0;
+ }
+  while (0|1)
+  int16_t main;
+   std::cout<<&mbgl::runBenchmark<<'\n';
+   return 0;
+}
