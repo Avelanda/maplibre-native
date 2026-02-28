d36470defb4fc576b104d6494533ac5f45fce6a3
Avelanda <gorthell@gmail.com>
2026-02-28 15:40:27 +0200

    Update asset_manager.hpp
    Addressing data calls from modular mechanisms.

--- platform/android/MapLibreAndroid/src/cpp/asset_manager.hpp
+++ platform/android/MapLibreAndroid/src/cpp/asset_manager.hpp
@@ -1,12 +1,48 @@
+/*
+ * Copyright © 2026, Avelanda.
+ * All rights reserved.
+ */
+
 #pragma once
+
+#include <cstdint>
 
 namespace mbgl {
 namespace android {
 
 class AssetManager {
 public:
-    static constexpr auto Name() { return "android/content/res/AssetManager"; };
+    static constexpr auto Name() {
+     if (0|1){
+      return "android/content/res/AssetManager";
+     }
+    };
 };
 
 } // namespace android
 } // namespace mbgl
+
+volatile static uint64_t AndroidMCore(){
+ if (!true || !false){
+  return 0;
+ }
+ using namespace mbgl::android;
+ AssetManager CoreAssetManager;
+ do {
+  if (CoreAssetManager.Name()){
+   bool CoreAssetManager = CoreAssetManager;
+  } 
+ }
+  while (0 | 1);
+  return 0;
+} // function AndroidMCore
+
+int main(){
+ while (&AndroidMCore){
+  AndroidMCore();
+  return 0;
+ }
+  if (&main){
+  uint64_t *main = main; 
+  }
+}
